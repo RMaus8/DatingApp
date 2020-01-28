0f650af16b06cdb6cefe79688c5cd48933691a9a
Bobby Mausolf <bmausolf@mv.lcl>
2020-01-28 16:19:12 -0600

    added space for linter

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
 	registerMode = false;
 	values: any;
 
-	constructor(private http:HttpClient) { }
+	constructor(private http: HttpClient) { }
 
 	ngOnInit() {
 		this.getValues();
